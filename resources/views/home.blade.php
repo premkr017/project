e4fc82d7e60337e93324ca60e6cd9a1a68eee8d0
Prem Kumar <premkr017@gmail.com>
2026-07-07 08:33:55 +0530

    Add form to home view for user input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,14 @@
 <div class="bg-white p-8 rounded-xl shadow-md w-full max-w-xl  text-center">
     <!-- Tailwind classes ka use karein -->
     <h1 class="text-indigo-700 font-bold text-4xl"> Welcome to the home Page!</h1>
+
+    <form class="flex item-center justify center space-x-2" ">
+
+        <input type="text" name="name" placeholder="Enter your name" class="border-2 border-black-300 rounded px-4 py-2 w-full mb-4 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+
+        <button type="submit" class="bg-indigo-700 text-white px-4 py-2 rounded hover:bg-indigo-800 transition duration-300">Click Me</button>
+    </form>
+
 </div>
 
 </body>

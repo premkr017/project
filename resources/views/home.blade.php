d8bc006575326eb26a8951ae103cbb2e16b35203
Prem Kumar <premkr017@gmail.com>
2026-07-07 10:41:04 +0530

    Update page title and add button for URL shortening in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>test page</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -15,6 +15,7 @@
 
         <form class="flex items-center justify-center space-x-2">
             <input type="text" name="org_url" class="w-full border border-blur-400 px-4 py-2 rounded-2xl focus:ring-2">
+            <input type="button" value="Shorten URL" class="bg-teal-500 text-blue px-4 py-2 rounded-2xl hover:bg-teal-600">
         </form>
     </div>
 </body>

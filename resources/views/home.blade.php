c34696065c2a0d0c64f803b37aae11fa8400a8a0
Prem Kumar <premkr017@gmail.com>
2026-07-07 08:42:44 +0530

    Refactor home view layout and update form for URL input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Home</title>
-    <!-- Laravel 12 me Tailwind ko link karne ke liye ye line add karein -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="bg-gray-100 h-screen flex items-center justify-center">
-<div class="bg-white p-8 rounded-xl shadow-md w-full max-w-xl  text-center">
-    <!-- Tailwind classes ka use karein -->
-    <h1 class="text-indigo-700 font-bold text-4xl"> Welcome to the home Page!</h1>
+    <div class="bg-white p-8 rounded-xl shadow-md w-full max-w-xl text-center">
+        <h1 class="text-teal-700 font-bold text-4xl">Laravel URL Shortner</h1>
 
-    <form class="flex item-center justify center space-x-2" ">
-
-        <input type="text" name="name" placeholder="Enter your name" class="border-2 border-black-300 rounded px-4 py-2 w-full mb-4 focus:outline-none focus:ring-2 focus:ring-indigo-500">
-
-        <button type="submit" class="bg-indigo-700 text-white px-4 py-2 rounded hover:bg-indigo-800 transition duration-300">Click Me</button>
-    </form>
-
-</div>
-
+        <form class="flex items-center justify-center space-x-2">
+            <input type="text" name="org_url" class="w-full border px-4 py-2 rounded-2xl">
+        </form>
+    </div>
 </body>
+
 </html>

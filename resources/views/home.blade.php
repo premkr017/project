8681d234b7001ecb6f2e77d84d6f225d0b19f396
Prem Kumar <premkr017@gmail.com>
2026-07-07 08:26:37 +0530

    Refactor home view and update route to return the correct view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,11 @@
     <!-- Laravel 12 me Tailwind ko link karne ke liye ye line add karein -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-slate-100 flex items-center justify-center h-screen m-0">
-
+<body class="bg-gray-100 h-screen flex items-center justify-center">
+<div class="bg-white p-8 rounded-xl shadow-md w-full max-w-xl  text-center">
     <!-- Tailwind classes ka use karein -->
-    <h1 class="text-2xl font-bold text-slate-800 bg-white p-6 rounded-lg shadow-md">
-        Welcome to the home Page!
-    </h1>
+    <h1 class="text-indigo-700 font-bold text-4xl"> Welcome to the home Page!</h1>
+</div>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\testController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('home');
 });
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
 
 Route::get('/test',[testController::class,'welcome']);
